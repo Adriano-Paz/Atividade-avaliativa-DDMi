098261e2795b07dd12f121ca1b10b6db893af221
Mateus <179388557+mateusjno@users.noreply.github.com>
2026-06-05 22:11:18 -0300

    feat: Adicionando códigos da página do carrinho (início))
    Cabeçalho está pronto

--- appdelivery/lib/carrinho_screen.dart
+++ appdelivery/lib/carrinho_screen.dart
@@ -10,6 +10,95 @@ class CarrinhoScreen extends StatefulWidget {
 class _CarrinhoScreenState extends State<CarrinhoScreen> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text(
+          'SACOLA',
+          style: TextStyle(
+            fontFamily: 'Ubuntu',
+            fontSize: 14,
+            fontWeight: FontWeight.w500,
+          ),
+        ),
+        centerTitle: true,
+        leading: IconButton(
+          onPressed: () {},
+          icon: const Icon(
+            Icons.keyboard_arrow_down,
+            color: Colors.red,
+            size: 30,
+          ),
+        ),
+        actions: [
+          TextButton(
+            onPressed: () {},
+            child: const Text(
+              'Limpar',
+              style: TextStyle(
+                color: Colors.red,
+                fontFamily: 'Ubuntu',
+                fontSize: 14,
+                fontWeight: FontWeight.w500,
+              ),
+            ),
+          ),
+        ],
+      ),
+      body: DefaultTextStyle(
+        style: const TextStyle(
+          fontFamily: 'Ubuntu',
+          fontSize: 16,
+          color: Colors.black,
+        ),
+        child: Column(
+          children: [
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 40),
+              child: Row(
+                children: [
+                  CircleAvatar(
+                    radius: 22,
+                    foregroundImage: AssetImage('images/bkcircleavatar.png'),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 15),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Text(
+                          "Burguer King - Ibiúna",
+                          style: TextStyle(fontWeight: FontWeight.w500),
+                        ),
+                        TextButton(
+                          style: TextButton.styleFrom(
+                            padding: EdgeInsets.zero,
+                            minimumSize: Size.zero,
+                            tapTargetSize: MaterialTapTargetSize.shrinkWrap,
+                            alignment: Alignment.centerLeft,
+                          ),
+                          onPressed: () {},
+                          child: const Text(
+                            'Adicionar mais itens',
+                            style: TextStyle(
+                              color: Colors.red,
+                              fontFamily: 'Ubuntu',
+                              fontSize: 14,
+                              fontWeight: FontWeight.w600,
+                              height: 1.8,
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+      bottomNavigationBar: BottomAppBar(),
+    );
   }
 }
--- appdelivery/lib/main.dart
+++ appdelivery/lib/main.dart
@@ -1,9 +1,13 @@
+import 'package:appdelivery/carrinho_screen.dart';
 import 'package:appdelivery/home_screen.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MaterialApp(
-    home: HomeScreen(),
-    debugShowCheckedModeBanner: false,)
-    );
+  runApp(
+    MaterialApp(
+      home: CarrinhoScreen(),
+      //home: HomeScreen(),
+      debugShowCheckedModeBanner: false,
+    ),
+  );
 }

23695d32f119a663568883499732c5b1e9267c19
Mateus <179388557+mateusjno@users.noreply.github.com>
2026-06-06 23:18:27 -0300

    feat: Finalizando página do carrinho
    Finalizada a página do carrinho e inserido os links de acesso entre as páginas.

--- appdelivery/lib/main.dart
+++ appdelivery/lib/main.dart
@@ -1,12 +1,11 @@
-import 'package:appdelivery/carrinho_screen.dart';
 import 'package:appdelivery/home_screen.dart';
 import 'package:flutter/material.dart';
 
 void main() {
   runApp(
     MaterialApp(
-      home: CarrinhoScreen(),
-      //home: HomeScreen(),
+      //home: CarrinhoScreen(),
+      home: HomeScreen(),
       debugShowCheckedModeBanner: false,
     ),
   );
